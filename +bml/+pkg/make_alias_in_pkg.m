be35ec45b5d0dc5a170851abcf91cc6f9fd09086
YulKang <yul.kang.on@gmail.com>
2016-09-11 16:56:40 -0400

    make_alias_in_pkg demands the target path to be on path

--- +bml/+pkg/make_alias_in_pkg.m
+++ +bml/+pkg/make_alias_in_pkg.m
@@ -89,6 +89,9 @@ end
 
 % Copy the help section.
 [~,name0] = fileparts(file0);
+if ~exist(name0, 'file')
+    error('%s is not on path! help section will not be copied!');
+end
 c = strsplit(help(name0), sprintf('\n'));
 % help() inserts a blank line at the end. Cancel it.
 if isempty(c{end}), c = c(1:(end-1)); end 
