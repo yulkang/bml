3dce24c73887f00a2c9b79e1812f0b03ab321d62
YulKang <yul.kang.on@gmail.com>
2016-09-16 11:43:01 -0400

    updated help

--- +bml/+args/factorizeC.m
+++ +bml/+args/factorizeC.m
@@ -7,10 +7,73 @@ function varargout = factorizeC(varargin)
 % in which case the fields will change values together.
 %
 % fields: fields to include or exclude
-% to_exclude
-% : if true, exclude the specified fields.
-% : if false (default), include only the specified fields.
+% to_fix
+% : if true, fix the specified fields (do not factorize them).
+% : if false (default), factorize only the specified fields.
+%   When fixing a field, the field is fixed to value{1}, ignoring the rest.
 %
-% EXAMPLE:
+% EXAMPLES:
+% %% Factorize a and the b-c pair.
+% [Ss, n] = factorizeC({
+%     'a', {1, 2}
+%     {'b', 'c'}, {{10, 100}, {20, 200}}
+%     });
+% for ii = 1:n
+%     disp(Ss(ii));
+% end
+%
+% %% RESULTS:
+%     a: 1
+%     b: 10
+%     c: 100
 % 
+%     a: 1
+%     b: 20
+%     c: 200
+% 
+%     a: 2
+%     b: 10
+%     c: 100
+% 
+%     a: 2
+%     b: 20
+%     c: 200
+%
+% %% Factorize a only
+% [Ss, n] = factorizeC({
+%     'a', {1, 2}
+%     {'b', 'c'}, {{10, 100}, {20, 200}}
+%     }, 'a');
+% for ii = 1:n
+%     disp(Ss(ii));
+% end
+% 
+% %% RESULTS:
+%     a: 1
+%     b: 10
+%     c: 100
+% 
+%     a: 2
+%     b: 10
+%     c: 100
+%
+% %% Factorize all but a
+% [Ss, n] = factorizeC({
+%     'a', {1, 2}
+%     {'b', 'c'}, {{10, 100}, {20, 200}}
+%     }, 'a', true);
+% for ii = 1:n
+%     disp(Ss(ii));
+% end
+%
+% %% RESULTS:
+%     b: 10
+%     c: 100
+%     a: 1
+% 
+%     b: 20
+%     c: 200
+%     a: 1
+% 
+% See also: factorizeS, factorize
 [varargout{1:nargout}] = factorizeC(varargin{:});
