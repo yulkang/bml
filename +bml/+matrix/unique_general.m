829c29e0eab461fc6bed8d993e382309dea4113d
YulKang <yul.kang.on@gmail.com>
2016-09-17 04:21:54 -0400

    varargin2C2 added and works

--- +bml/+matrix/unique_general.m
+++ +bml/+matrix/unique_general.m
@@ -1,12 +1,22 @@
 function varargout = unique_general(varargin)
-% v = unique_general(v0)
+% Same as unique() but applies to any class. The order is always stable.
+% 
+% [v, ia, ic] = unique_general(v0)
 %
 % EXAMPLE:
-% >> bml.matrix.unique_general({2, 1, 4, 'a', 'b', 'a', 4})
-% ans = 
+% >> [c, ia, ic] = unique_general({2, 1, 4, 1, 'a', 'b', 'a', 4})
+% c = 
 %     [2]    [1]    [4]    'a'    'b'
-% 
-% >> bml.matrix.unique_general([2 1 4 3 4 1])
-% ans =
+% ia =
+%      1     2     3     5     6
+% ic =
+%      1     2     3     2     4     5     4     3% 
+%
+% >> [c, ia, ic] = unique_general([2 1 2 4 3 4 1])
+% c =
 %      2     1     4     3
+% ia =
+%      1     2     4     5
+% ic =
+%      1     2     1     3     4     3     2
 [varargout{1:nargout}] = unique_general(varargin{:});
--- bml/matrix/unique_general.m
+++ bml/matrix/unique_general.m
@@ -1,46 +1,73 @@
-function v = unique_general(v0)
-% v = unique_general(v0)
+function [c, ia, ic] = unique_general(a) % , varargin)
+% Same as unique() but applies to any class. The order is always stable.
+% 
+% [v, ia, ic] = unique_general(v0)
 %
 % EXAMPLE:
-% >> bml.matrix.unique_general({2, 1, 4, 'a', 'b', 'a', 4})
-% ans = 
+% >> [c, ia, ic] = unique_general({2, 1, 4, 1, 'a', 'b', 'a', 4})
+% c = 
 %     [2]    [1]    [4]    'a'    'b'
-% 
-% >> bml.matrix.unique_general([2 1 4 3 4 1])
-% ans =
+% ia =
+%      1     2     3     5     6
+% ic =
+%      1     2     3     2     4     5     4     3% 
+%
+% >> [c, ia, ic] = unique_general([2 1 2 4 3 4 1])
+% c =
 %      2     1     4     3
-
+% ia =
+%      1     2     4     5
+% ic =
+%      1     2     1     3     4     3     2
+     
 % 2016 (c) Yul Kang. hk2699 at columbia dot edu.
 
-if isempty(v0)
-    if iscell(v0)
-        v = {};
-    else
-        v = [];
-    end
+if isempty(a)
+    c = a;
+    ia = [];
+    ic = [];
     return;
 end
 
-n = numel(v0);
+% S = varargin2S(varargin, {
+%     'prioritize_last', false
+%     });
+
+n = numel(a);
 % v = repmat(feval(class(v0)), size(v0));
 
-is_unique = false(size(v0));
-is_unique(n) = true;
+is_unique = false(size(a));
+is_unique(1) = true;
+n_unique = 1;
+ic = ones(size(a));
 
-for ii = n:-1:1
+for ii = 2:n
     is_unique1 = true;
-    for jj = 1:(ii-1)
-        if isequal(v0(jj), v0(ii))
+    
+    jjs = hVec(find(is_unique));
+%     if S.prioritize_last
+%         jjs = flip(jjs, 2);
+%     end
+    
+    for jj = jjs
+        if isequal(a(jj), a(ii))
             is_unique1 = false;
             break;
         end
     end
     is_unique(ii) = is_unique1;
+    if is_unique1
+        n_unique = n_unique + 1;
+        ic(ii) = n_unique;
+    else
+        ic(ii) = ic(jj);
+    end
 end
-v = v0(is_unique);
-if all(is_unique(:))
-    v = reshape(v, size(v0));
-end
+ia = find(is_unique);
+c = a(is_unique);
+% if all(is_unique(:))
+%     c = reshape(c, size(a));
+% end
 
 
 % v(1) = v0(1);
