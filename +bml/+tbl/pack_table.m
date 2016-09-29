3ae943c173cc6653478ffa4b9c9ecbf613a07ab4
YulKang <yul.kang.on@gmail.com>
2016-09-29 13:16:53 -0400

    added set_mat_by_col and pack_table

---
+++ +bml/+tbl/pack_table.m
@@ -0,0 +1,15 @@
+function tbl = pack_table(varargin)
+% tbl = pack_table(var1, var2, ...)
+%
+% same as table(var1, var2, ...) except matrices are set by set_mat_by_col.
+
+% 2016 (c) Yul Kang. hk2699 at columbia dot edu.
+
+n_col = numel(varargin);
+tbl = table;
+
+for i_col = 1:n_col
+    name = inputname(i_col);
+    
+    tbl = bml.tbl.set_mat_by_col(tbl, name, varargin{i_col});
+end
