9923d112fbda08a715d96e3631306d05a0d39d2f
YulKang <yul.kang.on@gmail.com>
2016-09-30 09:32:57 -0400

    tried not adding number for vec but commented out

--- +bml/+tbl/pack_table.m
+++ +bml/+tbl/pack_table.m
@@ -1,7 +1,8 @@
 function tbl = pack_table(varargin)
 % tbl = pack_table(var1, var2, ...)
 %
-% same as table(var1, var2, ...) except matrices are set by set_mat_by_col.
+% same as table(var1, var2, ...) except matrices 
+% are set by set_mat_by_col.
 
 % 2016 (c) Yul Kang. hk2699 at columbia dot edu.
 
@@ -11,5 +12,9 @@ tbl = table;
 for i_col = 1:n_col
     name = inputname(i_col);
     
-    tbl = bml.tbl.set_mat_by_col(tbl, name, varargin{i_col});
+%     if size(varargin{i_col}, 2) > 1
+        tbl = bml.tbl.set_mat_by_col(tbl, name, varargin{i_col});
+%     else
+%         tbl.(name) = varargin{i_col};
+%     end
 end
