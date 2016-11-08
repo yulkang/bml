70b0ddd2168f73a5d82d7eab27f027bacf863315
YulKang <yul.kang.on@gmail.com>
2016-11-08 11:37:01 -0500

    added invgausspdf_ms

---
+++ +bml/+distrib/invgausspdf_ms.m
@@ -0,0 +1,25 @@
+function p = invgausspdf_ms(x, mu, sd, robust_dim)
+% p = invgausspdf_ms(x, mu, sd, robust_dim)
+persistent pd
+
+if nargin < 4
+    robust_dim = 0;
+end
+
+if isempty(pd)
+    pd = makedist('InverseGaussian');
+end
+pd.mu = mu;
+pd.lambda = mu.^3 ./ sd.^2;
+
+if robust_dim == 0
+    p = pd.pdf(x);
+elseif robust_dim == 1
+    dx = x(2) - x(1);
+    x = [x(1) - dx / 2; x(:) + dx / 2];
+    
+    p = diff(pd.cdf(x));
+else
+    error('Unsupported robust_dim=%s\n', robust_dim);
+end
+end
