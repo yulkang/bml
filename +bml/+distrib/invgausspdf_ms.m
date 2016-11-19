ca02010e569922dc5314d7d5b3b6f16e4576ba03
YulKang <yul.kang.on@gmail.com>
2016-11-19 17:49:02 -0500

    debugged invgausspdf_ms for robust_dim=1 to avoid NaN

--- +bml/+distrib/invgausspdf_ms.m
+++ +bml/+distrib/invgausspdf_ms.m
@@ -15,10 +15,15 @@ pd.lambda = mu.^3 ./ sd.^2;
 if robust_dim == 0
     p = pd.pdf(x);
 elseif robust_dim == 1
-    dx = x(2) - x(1);
-    x = [x(1) - dx / 2; x(:) + dx / 2];
+%     dx = x(2) - x(1);
+%     x = [x(1) - dx / 2; x(:) + dx / 2];
+%     
+%     p = diff(pd.cdf(x));
     
-    p = diff(pd.cdf(x));
+    if any(isnan(p))
+        p = pd.pdf(x);
+        p = bsxfun(@rdivide, p, sum(p));
+    end 
 else
     error('Unsupported robust_dim=%s\n', robust_dim);
 end
