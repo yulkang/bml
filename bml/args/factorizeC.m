1224380646a76da60d7ed517a14e47b54b4a34ab
YulKang <yul.kang.on@gmail.com>
2016-12-09 11:25:50 -0500

    factorizeC handles empty input and returns struct with no field and n=1

--- bml/args/factorizeC.m
+++ bml/args/factorizeC.m
@@ -82,6 +82,13 @@ function [Ss, n] = factorizeC(C, fields, to_fix)
 C = S2C2(C);
 m = size(C,1);
 
+% Handle empty cases
+if isempty(C)
+    Ss = struct;
+    n = 1;
+    return;
+end
+
 if nargin < 2
     fields = C(:,1);
 elseif ~iscell(fields)
