eac444a0407e312cb2d7e67dc7e80714c7fd00d9
YulKang <yul.kang.on@gmail.com>
2017-01-02 10:47:16 -0500

    added jsdivergence and kldivergence

---
+++ +bml/+math/kldivergence.m
@@ -0,0 +1,9 @@
+function d = kldivergence(p1, p2)
+% If multidimensional, works on the first dimension. Uses log2().
+%
+% d = kldivergence(p1, p2)
+% : computes D_KL(p1 || p2)
+
+assert(isequal(size(p1), size(p2)));
+
+d = sum(p1 .* (log2(p1) - log2(p2)));
