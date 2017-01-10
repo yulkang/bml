6d60555d1dd800096423f6f823b069e4b286a4d0
YulKang <yul.kang.on@gmail.com>
2017-01-10 10:15:02 -0500

    gltitle defaults to shift=0

--- bml/plot/gltitle.m
+++ bml/plot/gltitle.m
@@ -1,20 +1,35 @@
-function hgl = gltitle(h, op, t, varargin)
-% Column and row titles
+function h_ax = gltitle(h_ax0, op, t, varargin)
+% Column and row titles.
+% Makes a new axes that encompasses the combined areas of h_ax0,
+% and adds a title to the new axes.
 %
-% hgl = gltitle(h, op, t, varargin)
+% h_ax = gltitle(h, op, t, varargin)
 %
-% h     : Array of axes
+% h_ax0 : Array of axes
 % op    : 'all', 'row', 'col'
 % t     : Title. In case of 'all', a string. In case of 'row' or 'col', a cell array of strings.
-% shift : [xshift, yshift] or [xshift, yshift, zshift]. Defaults to [0.05, -0.05].
+% shift : [xshift, yshift] or [xshift, yshift, zshift].
+%   Defaults to [0, 0].
+%   Use bml.plot.position_subplots instead of specifying shift
+%   for flexible positioning.
+%   If nonzero shift is used, [0.05, -0.05] is often reasonable.
+%         Use bml.plot.position_subplots first for flexible positioning.
+% title_args : arguments fed to title().
+
+% 2016-2017 (c) Yul Kang. hk2699 at columbia dot edu.
 
 S = varargin2S(varargin, {
-    'shift', [0.05, -0.05]
+    ... % shift : [xshift, yshift] or [xshift, yshift, zshift].
+    ... %   Defaults to [0, 0].
+    ... %   Use bml.plot.position_subplots instead of specifying shift
+    ... %   for flexible positioning.
+    ... %   If nonzero shift is used, [0.05, -0.05] is often reasonable.
+    'shift', [0, 0] 
     'title_args', {}
     });
 
 if ~isempty(S.shift)
-    shiftpos(h, S.shift);
+    shiftpos(h_ax0, S.shift);
 end
 if ischar(t), t = {t}; end
         
@@ -27,14 +42,14 @@ switch op
             'Position',     [0.5, 1.06, 0.5]
             });
         
-        hgl = glaxes(h, 'title', t, C{:});
+        h_ax = glaxes(h_ax0, 'title', t, C{:});
         
     case 'row'
-        n = size(h, 1);
+        n = size(h_ax0, 1);
         if verLessThan('matlab', '8.4')
-            hgl = zeros(1, n);
+            h_ax = zeros(1, n);
         else
-            hgl = gobjects(1, n);
+            h_ax = gobjects(1, n);
         end
         
         C = varargin2C(S.title_args, {
@@ -47,15 +62,15 @@ switch op
             });
         
         for ii = 1:n
-            hgl(ii) = glaxes(h(ii,:), 'ylabel', t{ii}, C{:});
+            h_ax(ii) = glaxes(h_ax0(ii,:), 'ylabel', t{ii}, C{:});
         end
         
     case 'col'
-        n = size(h, 2);
+        n = size(h_ax0, 2);
         if verLessThan('matlab', '8.4')
-            hgl = zeros(1, n);
+            h_ax = zeros(1, n);
         else
-            hgl = gobjects(1, n);
+            h_ax = gobjects(1, n);
         end
         
         C = varargin2C(S.title_args, {
@@ -65,6 +80,6 @@ switch op
             });
         
         for ii = 1:n
-            hgl(ii) = glaxes(h(:,ii), 'title', t{ii}, C{:});
+            h_ax(ii) = glaxes(h_ax0(:,ii), 'title', t{ii}, C{:});
         end
 end
