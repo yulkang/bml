182ba6606efe9dfda86f264e1ae75a73ac3454c7
YulKang <yul.kang.on@gmail.com>
2017-03-13 15:59:17 -0400

    allowed divergence to output d_sep

--- +bml/+math/jsdivergence.m
+++ +bml/+math/jsdivergence.m
@@ -1,7 +1,14 @@
-function d = jsdivergence(p1, p2)
+function [d, d_sep] = jsdivergence(p1, p2)
 % Jensen-Shannon divergence between two distributions.
 %
-% d = jsdivergence(p1, p2)
+% [d, d_sep] = jsdivergence(p1, p2)
 
 m = (p1 + p2) ./ 2;
+
 d = (bml.math.kldivergence(p1, m) + bml.math.kldivergence(p2, m)) ./ 2;
+
+if nargout >= 2
+    [~, kl1] = bml.math.kldivergence(p1, m);
+    [~, kl2] = bml.math.kldivergence(p2, m);
+    d_sep = (kl1 + kl2) ./ 2;
+end    
