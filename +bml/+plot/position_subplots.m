89385680d3e0c757db1a6527d944bc4fb4352b04
YulKang <yul.kang.on@gmail.com>
2017-04-05 13:52:32 -0400

    position_subplots works with height and width options

--- +bml/+plot/position_subplots.m
+++ +bml/+plot/position_subplots.m
@@ -25,6 +25,11 @@ S = varargin2S(varargin, {
     'margin', nan(1,4) 
     'btw_row', 0.05 % if vector, btw_row(1) is between rows 1 and 2.
     'btw_col', 0.05 % if vector, btw_col(1) is between columns 1 and 2.
+    ...
+    'height', [] % if a nonempty vector, determines the relative height
+    ...          % from top.
+    'width', []  % if a nonempty vector, determines the relative width
+    ...          % from left.
     });
 n_row = size(ax, 1);
 n_col = size(ax, 2);
@@ -41,20 +46,38 @@ end
 S.btw_row = bml.matrix.rep2fit(S.btw_row(:), [n_row - 1, 1]);
 S.btw_col = bml.matrix.rep2fit(S.btw_col(:), [n_col - 1, 1]);
 
+width_total = 1 - sum(S.margin([1, 3])) - sum(S.btw_col);
+height_total = 1 - sum(S.margin([2, 4])) - sum(S.btw_row);
 
-width = (1 - sum(S.margin([1, 3])) - sum(S.btw_col)) / n_col;
-height = (1 - sum(S.margin([2, 4])) - sum(S.btw_row)) / n_row;
+if isempty(S.height)
+    height = height_total / n_row + zeros(1, n_row);
+else
+    assert(isvector(S.height));
+    assert(numel(S.height) == n_row);
+    height = S.height / sum(S.height) * height_total;
+end
+
+if isempty(S.width)
+    width = width_total / n_col + zeros(1, n_col);
+else
+    assert(isvector(S.width));
+    assert(numel(S.width) == n_col);
+    width = S.width / sum(S.width) * width_total;
+end
 
 for i_row = 1:n_row
     for i_col = 1:n_col
         left = S.margin(1) ...
              + sum(S.btw_col(1:(i_col - 1))) ...
-             + width * (i_col - 1);
+             + sum(width(1:(i_col - 1)));
+%              + width * (i_col - 1);
         bottom = S.margin(2) ...
                + sum(S.btw_row(i_row:(n_row - 1))) ...
-               + height * (n_row - i_row);
-        
-        set(ax(i_row, i_col), 'Position', [left, bottom, width, height]);
+               + sum(height((i_row+1):n_row));
+%                + height * (n_row - i_row);
+
+        set(ax(i_row, i_col), ...
+            'Position', [left, bottom, width(i_col), height(i_row)]);
     end
 end
 end
