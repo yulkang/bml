f7a2ead25494cb09fca40d3e12f3247dd7e4bbd8
YulKang <yul.kang.on@gmail.com>
2017-12-28 15:48:03 -0500

    made delete_null_files work when folder field is absent in info

--- +bml/+file/delete_null_files.m
+++ +bml/+file/delete_null_files.m
@@ -3,13 +3,25 @@ function delete_null_files(pth)
 %
 % delete_null_files(pth)
 
+if exist(pth, 'dir')
+    pth0 = pth;
+else
+    pth0 = fileparts(pth);
+    assert(exist(pth0, 'dir') ~= 0, 'Cannot parse the folder name!');
+end
+    
 info = dir(pth);
 is_null = ([info.bytes] == 0) & (~[info.isdir]);
 info_null = info(is_null);
 n_null = numel(info_null);
 for ii = 1:n_null
     info1 = info_null(ii);
-    file = fullfile(info1.folder, info1.name);
+    if isfield(info1, 'folder')
+        pth1 = info1;
+    else
+        pth1 = pth0;
+    end
+    file = fullfile(pth1, info1.name);
     delete(file);
     fprintf('Deleted %d/%d: %s\n', ii, n_null, file);
 end
