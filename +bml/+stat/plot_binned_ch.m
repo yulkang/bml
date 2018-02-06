b4a910880207062e44d56e0e2a7074caf7560bcd
YulKang <yul.kang.on@gmail.com>
2018-02-06 15:49:56 +0000

    plot_binned_ch filters out NaN in x0 and ch

--- +bml/+stat/plot_binned_ch.m
+++ +bml/+stat/plot_binned_ch.m
@@ -2,10 +2,22 @@ function [h, y_bin, x, x_bin] = plot_binned_ch(x0, ch, varargin)
 % [h, y_bin, x, x_bin] = plot_binned_ch(x0, ch, varargin)
 S = varargin2S(varargin, {
     'n_bin', 9
-    'plot_args', {'o'};
+    'plot_args', {};
+    });
+S.plot_args = varargin2plot(S.plot_args, {
+    'o'
     });
 
+incl = ~isnan(x0) & ~isnan(ch);
+x0 = x0(incl);
+ch = ch(incl);
+
 [x_bin, ~, x] = quantilize(x0, S.n_bin);
+
 y_bin = accumarray(x_bin, ch, [], @nanmean);
-h = plot(x, y_bin, S.plot_args{:});
+% try
+    h = plot(x, y_bin, S.plot_args{:});
+% catch
+%     keyboard;
+% end
 end
