54cd670cf080ab3ed83979ab42d2eff70c1e1a4d
YulKang <yul.kang.on@gmail.com>
2018-02-06 15:49:38 +0000

    added help to glmfit_lapse

--- +bml/+stat/glmfit_lapse.m
+++ +bml/+stat/glmfit_lapse.m
@@ -1,16 +1,39 @@
 function [b, res] = glmfit_lapse(X0, y0, varargin)
+% [b, res] = glmfit_lapse(X0, y0, varargin)
+%
 % b(1) : offset
 % b(1 + (1:size(X0,2))): beta
 % b(end) : logit(p_lapse)
 %
+% X0 : design matrix (trial, regressor)
 % y0 : logical column vector
-
+%
+% OPTIONS
+% 'lapse0', logit(1e-3)
+% 'lapse_min', logit(1e-5)
+% ...
+% ... 'lapse_max'
+% ... if using logit(1 - 1e-5), consider fixing slope by
+% ... setting slope0 = slope_min = slope_max
+% 'lapse_max', logit(0.2) 
+% 'bias0', 0
+% 'bias_min', -1
+% 'bias_max', 1
+% 'slope0', 5
+% 'slope_min', -5
+% 'slope_max', 50
+% 'opt', {}
+    
 % 2016 Yul Kang. hk2699 at columbia dot edu.
 
 S = varargin2S(varargin, {
     'lapse0', logit(1e-3)
     'lapse_min', logit(1e-5)
-    'lapse_max', logit(0.2)
+    ...
+    ... 'lapse_max'
+    ... if using logit(1 - 1e-5), consider fixing slope by
+    ... setting slope0 = slope_min = slope_max
+    'lapse_max', logit(0.2) 
     'bias0', 0
     'bias_min', -1
     'bias_max', 1
@@ -24,14 +47,17 @@ n_tr = size(X0, 1);
 n_col = size(X0, 2);
 assert(iscolumn(y0) && length(y0) == n_tr);
 
-b0 = [S.bias0, zeros(1, n_col) + S.slope0, logit(S.lapse0)];
+b0 = [S.bias0, zeros(1, n_col) + S.slope0, S.lapse0];
 lb = [S.bias_min, zeros(1, n_col) + S.slope_min, S.lapse_min];
 ub = [S.bias_max, zeros(1, n_col) + S.slope_max, S.lapse_max];
 
 opt = optimoptions('fmincon', S.opt{:});
 
+f = @(b) -bml.stat.glmlik_lapse(b, X0, y0);
+disp(f(b0)); % DEBUG
+
 [b, fval, exitflag, output, lambda, grad, hessian] = ...
-    fmincon(@(b) -bml.stat.glmlik_lapse(b, X0, y0), ...
+    FminconReduce.fmincon(f, ...
         b0, [], [], [], [], lb, ub, [], opt);
 
 se = sqrt(diag(inv(hessian)));
