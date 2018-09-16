d00d31a6068e9b4bd178d88a68358983c5c637fa
YulKang <yul.kang.on@gmail.com>
2018-09-16 13:32:58 +0100

    beautify_tick now supports min_n_tick

--- +bml/+plot/beautify_tick.m
+++ +bml/+plot/beautify_tick.m
@@ -22,6 +22,7 @@ S = varargin2S(varargin, {
         0:0.01:5
         0:0.001:5
         }
+    'min_n_tick', 2
     });
 
 xy = upper(xy);
@@ -29,7 +30,7 @@ lim = get(ax, [xy 'Lim']);
 n_tick = numel(S.tick);
 for i_tick = 1:n_tick
     tick = S.tick{i_tick};
-    if nnz((lim(1) <= tick) & (tick <= lim(2))) >= 2
+    if nnz((lim(1) <= tick) & (tick <= lim(2))) >= S.min_n_tick
         break;
     end
 end
