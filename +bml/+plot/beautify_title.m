9e60b58dbc844354e347ac9415c861addd9a32cc
YulKang <yul.kang.on@gmail.com>
2018-10-03 10:17:15 +0100

    beautify_title can get options to bml.str.wrap_text

--- +bml/+plot/beautify_title.m
+++ +bml/+plot/beautify_title.m
@@ -1,4 +1,4 @@
-function str = beautify_title(str)
+function str = beautify_title(str, varargin)
 % str = beautify_title(str0)
-str = bml.str.wrap_text(bml.plot.strrep_label(str));
+str = bml.str.wrap_text(bml.plot.strrep_label(str), varargin{:});
 end
