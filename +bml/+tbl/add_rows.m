d3e8216bf7546db50fa85ab4cc20de0ec2f51c9f
YulKang <yul.kang.on@gmail.com>
2018-11-01 22:31:04 +0000

    debugged tbl.add_rows

--- +bml/+tbl/add_rows.m
+++ +bml/+tbl/add_rows.m
@@ -1,9 +1,10 @@
 function tbl = add_rows(tbl, tbl2)
 % tbl = add_rows(tbl, tbl2)
+
 n1 = size(tbl, 1);
-n2 = size(tbl, 2);
+n2 = size(tbl2, 1);
 
 for col = tbl2.Properties.VariableNames(:)'
-    col_width = size(tbl2.col, 2);
-    tbl.(col{1})(n1 + (1:n2), 1:col_width) = tbl2.col;
+    col_width = size(tbl2.(col{1}), 2);
+    tbl.(col{1})(n1 + (1:n2), 1:col_width) = tbl2.(col{1});
 end
