e3ed9f367e5bd6e787fcf5e70cde28da95399661
YulKang <yul.kang.on@gmail.com>
2019-02-15 23:32:34 +0000

    bml.tbl.set_rows now works with structs

--- +bml/+tbl/set_rows.m
+++ +bml/+tbl/set_rows.m
@@ -3,10 +3,17 @@ if islogical(ix_rows)
     ix_rows = find(ix_rows);
 end
 assert(numel(ix_rows) == size(tbl1, 1));
-for ii = ix_rows(:)'
-    for col = tbl1.Properties.VariableNames(:)'
-        col_width = size(tbl1.(col{1}), 2);
-        tbl.(col{1})(ii, 1:col_width) = tbl1.(col{1});
-    end
+if isstruct(tbl1)
+    fs = fieldnames(tbl1);
+else
+    fs = tbl1.Properties.VariableNames;
+end
+for col = fs(:)'
+    col_width = size(tbl1.(col{1}), 2);
+    tbl.(col{1})(ix_rows, 1:col_width) = tbl1.(col{1});
+%     for ii = sort(ix_rows(:)', 'descend')
+%         col_width = size(tbl1.(col{1}), 2);
+%         tbl.(col{1})(ii, 1:col_width) = tbl1.(col{1})(ii,:);
+%     end
 end
 end
